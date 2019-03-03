641a68fb96bb8aa368dc2ae093dff74497479b94
Sanjay Sai <shachsan@hotmail.com>
2019-03-03 13:41:22 -0500

    done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let min=array[0];
+    let index;
+    array.forEach((ele, i)=>{
+        if (ele<min){
+            min=ele;
+            index=i;
+        }
+    })
+    array.splice(index,1)
+    return min;
 }
 
 function insertionSort(array){
-
+    let sortedArry=[];
+    while(array.length>=1){
+        let min=findMinAndRemove(array)
+        sortedArry.push(min)
+    }
+    return sortedArry;
 }
+
